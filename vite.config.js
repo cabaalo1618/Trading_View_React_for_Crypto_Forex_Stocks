d2b59b2d0566146446e5d3e16db3a70580944d4b
Thiago Monteiro <blackmillenium@gmail.com>
2026-02-19 19:57:00 -0300

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Trading_View/",Trading_View_React_for_Crypto_Forex_Stocks 
 })

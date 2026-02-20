0bd3b9641c7510dea1a9361f5e28a91566d86e30
Thiago Monteiro <blackmillenium@gmail.com>
2026-02-20 17:03:39 -0300

    Stable front-end version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Trading_View_React_for_Crypto_Forex_Stocks/',
   plugins: [react()],
-  base: "/Trading_View/",Trading_View_React_for_Crypto_Forex_Stocks 
 })
